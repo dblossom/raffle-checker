5512bbc9b12fd059372235b28f92e9e0f0fa377d
dblossom <daniel.blossom@gmail.com>
2021-03-23 18:43:19 -0400

    added a form that can send a list of tickets, right now it just displays what was inputed, next is to process the list and return the result. After that work on file parsing from *.csv files, which would be a backend change too.

--- raffleapp.py
+++ raffleapp.py
@@ -1,20 +1,34 @@
-from rafflechecker import RaffleChecker
-from flask import Flask
+#from rafflechecker import RaffleChecker
+from flask import Flask, render_template, redirect, url_for, request
 import random
-app = Flask(__name__)
+app = Flask(__name__, template_folder='.')
 
 @app.route('/')
 def index():
-    my_numbers = ["1884","1930","2487","2816"]
+    return render_template("index.html")
 
-    index = 1
-    while index < 5000:
-        index = index + 1
-        ran = random.randint(0,9999)
-        my_numbers.append(str(ran))
+@app.route("/rafflechecker", methods=["POST","GET"])
+def rafflechecker():
+    if request.method == "POST":
+        mylist = request.form["tickets"]
+        return redirect(url_for("raffleresult",mylst=mylist))
+    else:
+        return render_template("rafflechecker.html")
+    #my_numbers = ["1884","1930","2487","2816"]
 
-    rc = RaffleChecker(my_numbers)
-    rc.collect_winning_numbers()
-    a = rc.check_winner()
-    return a
-#    return 'Hello, World!'
+    #index = 1
+    #while index < 5000:
+    #    index = index + 1
+    #    ran = random.randint(0,9999)
+    #    my_numbers.append(str(ran))
+
+    #rc = RaffleChecker(my_numbers)
+    #rc.collect_winning_numbers()
+    #a = rc.check_winner()
+    #return a
+@app.route("/raffleresults<mylst>")
+def raffleresult(mylst):
+    return f"<p>{mylst}</p>"
+    #return render_template("raffleresults.html",mylst)
+if __name__ == "main__":
+    app.run(debug=True)
