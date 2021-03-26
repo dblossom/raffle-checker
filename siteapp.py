d7e08ad24f39a36c7cbbbaebd41c1250ba58d4ab
Dan Blossom <daniel.blossom@gmail.com>
2021-03-26 16:05:49 -0400

    redirects output to raffleresults.html

--- siteapp.py
+++ siteapp.py
@@ -1,4 +1,4 @@
-#from rafflechecker import RaffleChecker
+from rafflechecker import RaffleChecker
 from flask import Flask, render_template, redirect, url_for, request
 import random
 app = Flask(__name__, template_folder='.')
@@ -11,7 +11,9 @@ def index():
 def rafflechecker():
     if request.method == "POST":
         mylist = request.form["tickets"]
-        return redirect(url_for("raffleresult",mylst=mylist))
+        #print(mylist)
+        #return render_template("raffleresults.html",mylist=mylist)
+        return redirect(url_for("raffleresult",mylist=mylist))
     else:
         return render_template("rafflechecker.html")
     #my_numbers = ["1884","1930","2487","2816"]
@@ -26,9 +28,12 @@ def rafflechecker():
     #rc.collect_winning_numbers()
     #a = rc.check_winner()
     #return a
-@app.route("/raffleresults<mylst>")
-def raffleresult(mylst):
-    return f"<p>{mylst}</p>"
-    #return render_template("raffleresults.html",mylst)
+@app.route("/raffleresults<mylist>")
+def raffleresult(mylist):
+    splitlist = mylist.split(",")
+    rc = RaffleChecker(splitlist)
+    rc.collect_winning_numbers()
+    win = rc.check_winner()
+    return render_template("raffleresults.html",win=win)
 if __name__ == "main__":
     app.run(debug=True)
